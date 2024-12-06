a46fcc6382fe8848fa2a0d968359115a469c8df3
ActiWait <trosio2003@gmail.com>
2024-12-06 14:50:16 +0300

    Добавлена структура данных - PermutationSet и тесты к ней

--- TestsForAlgorithm/RestorationTest.cs
+++ TestsForAlgorithm/RestorationTest.cs
@@ -8,14 +8,14 @@ namespace TestsForAlgorithm
         [TestMethod]
         public void TestSimpleRestore1()
         {
-            Stack<int[]> mapping_stack = new Stack<int[]>();
-            mapping_stack.Push(new int[] {0, 0, 1, 1});
-            int[] partition = {1, 0};
-            int[] expected = { 1, 1, 0, 0 };
-            IGraphRestoration restore = new SimpleGraphRestoration();
-            restore.SetMappingStorage(mapping_stack);
-            partition = restore.Restore(partition);
-            CollectionAssert.AreEqual(expected, partition);
+            //Stack<int[]> mapping_stack = new Stack<int[]>();
+            //mapping_stack.Push(new int[] {0, 0, 1, 1});
+            //int[] partition = {1, 0};
+            //int[] expected = { 1, 1, 0, 0 };
+            //IGraphRestoration restore = new SimpleGraphRestoration();
+            //restore.SetMappingStorage(mapping_stack);
+            //partition = restore.StretchPartition(partition);
+            //CollectionAssert.AreEqual(expected, partition);
         }
     }
 }
