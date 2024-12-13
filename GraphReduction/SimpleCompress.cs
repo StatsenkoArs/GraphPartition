22b8898b6f0392b093c9c123958a9e62beafed8a
ActiWait <trosio2003@gmail.com>
2024-12-13 15:29:15 +0300

    Поправлена работа SimpleCompress

--- GraphReduction/SimpleCompress.cs
+++ GraphReduction/SimpleCompress.cs
@@ -9,25 +9,20 @@ namespace GraphReduction
 {
     public class SimpleCompress : ICompress
     {
-        private int[] vertex_mapping;
-        private bool[] graph_vertex_used;  //Убрать? Учитывать это по заполненности шифра?
+        private int[]? vertex_mapping;
+        private bool[]? graph_vertex_used;  //Убрать? Учитывать это по заполненности шифра?
         private int _group;
-        public SimpleCompress()
-        {
-            vertex_mapping = Array.Empty<int>();
-            graph_vertex_used = Array.Empty<bool>();
-        }
         public int[] Compress(IGraph graph)
         {
-            vertex_mapping = new int[graph.CountEdges];
-            graph_vertex_used = new bool[graph.CountEdges];
-            for (int i = 0; i < graph.CountEdges; i++)
+            vertex_mapping = new int[graph.CountVertecies];
+            graph_vertex_used = new bool[graph.CountVertecies];
+            for (int i = 0; i < graph.CountVertecies; i++)
             {
                 graph_vertex_used[i] = false;
             }
             int group = 0;
             bool flag = true;
-            for (int i = 0; i < graph.CountEdges; i++)
+            for (int i = 0; i < graph.CountVertecies; i++)
             {
                 if (flag == false)
                 {
