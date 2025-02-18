bbeb6ae4353393185f511a336497dc002fd9e5ef
Dekim-49 <74243140+Dekim-49@users.noreply.github.com>
2025-02-18 17:21:56 +0300

    Update GrafFeduchiMatteus.cs

--- GraphOptimisation/GrafFeduchiMatteus.cs
+++ GraphOptimisation/GrafFeduchiMatteus.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Feduchi_Matteus_method_exampl_1
+namespace GraphOptimisation
 {
     public class FiducciaMattheysesMethod : IPartitionOptimisation
     {
