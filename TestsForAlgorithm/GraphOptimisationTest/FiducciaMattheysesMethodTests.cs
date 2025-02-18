43f6f86d9ed107674c53ba6c0e4f8ccf888dc7e7
Dekim-49 <74243140+Dekim-49@users.noreply.github.com>
2025-02-18 17:16:31 +0300

    Update FiducciaMattheysesMethodTests.cs

--- TestsForAlgorithm/GraphOptimisationTest/FiducciaMattheysesMethodTests.cs
+++ TestsForAlgorithm/GraphOptimisationTest/FiducciaMattheysesMethodTests.cs
@@ -72,54 +72,6 @@ namespace UnitTestProject1
             Assert.AreEqual(5, newR.Sum());
         }
 
-
-        //[TestMethod]
-        //public void Graph_30Vertex()
-        //{
-           
-        //    int[][] g = Method.ReadGraph("C:\\Users\\ASUS\\Desktop\\ННГУ\\ДИПЛОМ\\Попытка в методы\\graphs\\graph_30.txt", 30);
-        //    GraphSRC graph = new GraphSRC(g);
-
-
-        //    Generator generator = new Generator();
-        //    int[] r = generator.GenerateSubgrapf(30);
-        //    FiducciaMattheysesMethod f = new FiducciaMattheysesMethod();
-
-        //    int[] newR = new int[r.Length];
-        //    newR = f.OptimisePartition(graph, r, 1);
-        //    Assert.AreEqual(15, newR.Sum());
-        //}
-
-        //[TestMethod]
-        //public void Graph_50Vertex()
-        //{
-        //    Generator generator = new Generator();
-        //    int[][] g = Method.ReadGraph("C:\\Users\\ASUS\\Desktop\\ННГУ\\ДИПЛОМ\\Попытка в методы\\graphs\\graph_50.txt", 50);
-        //    GraphSRC graph = new GraphSRC(g);
-        //    int[] r = generator.GenerateSubgrapf(50);
-        //    FiducciaMattheysesMethod f = new FiducciaMattheysesMethod();
-
-        //    int[] newR = new int[r.Length];
-        //    newR = f.OptimisePartition(graph, r, 1);
-        //    Assert.AreEqual(25, newR.Sum());
-        //}
-
-        //[TestMethod]
-        //public void Graph_500Vertex()
-        //{
-        //    Generator generator = new Generator();
-        //    const int n = 500;
-        //    int[][] g = generator.Generate(n, n/2 + n/3, n-n/3, 3);
-        //    GraphSRC graph = new GraphSRC(g);
-        //    int[] r = generator.GenerateSubgrapf(500);
-        //    FiducciaMattheysesMethod f = new FiducciaMattheysesMethod();
-
-        //    int[] newR = new int[r.Length];
-        //    newR = f.OptimisePartition(graph, r, 1);
-        //    Assert.AreEqual(250, newR.Sum());
-        //}
-
-        
         [TestMethod]
         public void FiducciaMattheysesMethod_UnconnectedGraphAndNotEqualDistribution2Vertex_EqualDistribution()
         {
@@ -157,8 +109,6 @@ namespace UnitTestProject1
             //assert
             Assert.AreEqual(1, newR.Sum());
         }
-
-
 
 
     }
