c42f3e300e94c853730fc87c220f810363e5d112
ActiWait <trosio2003@gmail.com>
2025-02-19 15:08:32 +0300

    Добавлено хранение Структуры уровней графа

--- GraphRenumbering/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GraphRenumbering
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ GraphRenumbering/LevelStructSRC.cs
@@ -0,0 +1,26 @@
+﻿namespace GraphRenumbering
+{
+    internal class LevelStructSRC
+    {
+        private int[] _adjacentNums;
+        private int[] _adjacentLevels;
+        public LevelStructSRC(int[] adjacentNums, int[] adjacentLevels)
+        {
+            _adjacentLevels = adjacentLevels;
+            _adjacentNums = adjacentNums;
+        }
+        public int this[int levelNum, int adjacentNum]
+        {
+            get => _adjacentLevels[_adjacentNums[levelNum] + adjacentNum];
+        }
+
+        public int CountLevels => _adjacentNums.Length - 1;
+
+        public int Length => _adjacentLevels.Length;
+
+        public int GetNumVertexOnLevel(int levelNum)
+        {
+            return _adjacentNums[levelNum + 1] - _adjacentNums[levelNum];
+        }
+    }
+}
