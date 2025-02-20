b3792040eb5c71e384306aced28919a69462ee63
ActiWait <trosio2003@gmail.com>
2025-02-20 16:39:00 +0300

    Удалены неактуальыне тесты

--- TestsForAlgorithm/RenumGraphTest.cs
+++ TestsForAlgorithm/RenumGraphTest.cs
@@ -1,11 +1,5 @@
-﻿using GraphReduction;
+﻿using GraphRenumbering;
 using GraphRepresentation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestsForAlgorithm
 {
