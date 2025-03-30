c876da2fa9f11fae9740bf63ac4f7c3c09cafd16
ActiWait <trosio2003@gmail.com>
2025-03-30 18:18:12 +0300

    Добавлен новый сеточный генератор с прореживанием и тесты к нему

--- ExampleGenerator/GridGraphGenerator.cs
+++ ExampleGenerator/GridGraphGenerator.cs
@@ -1,4 +1,9 @@
-﻿namespace ExampleGenerator
+﻿using System.Drawing;
+using System.Net.Http.Headers;
+using System.Numerics;
+using System.Runtime.ExceptionServices;
+
+namespace ExampleGenerator
 {
     /// <summary>
     /// Класс для генератора графа на сетке
@@ -14,7 +19,7 @@
         public static int[][] Generate(int num_of_row, int num_of_col)
         {
             int[][] graph = new int[num_of_row * num_of_col][];
-            int[] tmp_adj_vertex =  new int[4];
+            int[] tmp_adj_vertex = new int[4];
             int vertex_num, vertex_degree;
             for (int r = 0; r < num_of_row; r++)
             {
@@ -36,7 +41,7 @@
                         vertex_degree++;
                     }
                     //Связь с вершиной справа
-                    if (c  + 1 != num_of_col) 
+                    if (c + 1 != num_of_col)
                     {
                         tmp_adj_vertex[vertex_degree] = vertex_num + 1;
                         vertex_degree++;
@@ -56,6 +61,104 @@
                 }
             }
             return graph;
+        }
+
+        public static int[,] GetExpandedMatrix(int num_of_row, int num_of_col)
+        {
+            return new int[num_of_row + 2, num_of_col + 2];
+        }
+        public static void FillExpandedMatrixBorder(int[,] matrix)
+        {
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                matrix[r, 0] = -1;
+                matrix[r, matrix.GetLength(1) - 1] = -1;
+            }
+            for (int c = 0; c < matrix.GetLength(1); c++)
+            {
+                matrix[0, c] = -1;
+                matrix[matrix.GetLength(0) - 1, c] = -1;
+            }
+        }
+        public static void AddRectangleHoleMatrix(int[,] matrix, 
+                                                  Point[] start_rect_point, 
+                                                  Point[] end_rect_point)
+        {
+            if (start_rect_point.Length != end_rect_point.Length) throw new Exception("start_rect_point.Length != end_rect_point.Length");
+            for (int i = 0; i < start_rect_point.Length && i < end_rect_point.Length; i++)
+            {
+                for (int r = start_rect_point[i].X; r <= end_rect_point[i].X; r++)
+                {
+                    for (int c = start_rect_point[i].Y; c <= end_rect_point[i].Y; c++)
+                    {
+                        matrix[r, c] = -1;
+                    }
+                } 
+            }
+        }
+        public static void NumberExpandedMatrixWithHole(int [,] matrix)
+        {
+            int count = 0;
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (matrix[r, c] != -1) matrix[r, c] = count++;
+                }
+            }
+        }
+
+        public static int[][] GetGraphFromMatrix(int [,] matrix)
+        {
+            Point[] directions = {new(-1, 0), new(0, -1), new(0, 1), new(1, 0)};
+            List<int> vertexes = new List<int>();
+            
+            int last_number_vetex = matrix[matrix.GetLength(0) - 2, matrix.GetLength(1) - 2];
+            int[][] graph = new int[last_number_vetex + 1][];
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (matrix[r, c] == -1) continue;
+                    foreach (Point d in directions)
+                    {
+                        int adj_vertexes = matrix[r + d.X, c + d.Y];
+                        if (adj_vertexes != -1) vertexes.Add(adj_vertexes);
+                    }
+                    graph[matrix[r, c]] = new int[vertexes.Count];
+                    for (int i = 0; i < vertexes.Count; i++)
+                    {
+                        graph[matrix[r, c]][i] = vertexes[i];
+                    }
+                    vertexes.Clear();   
+                }
+            }
+            return graph;
+        }
+
+        public static Point[] GetVertexCoordFromMatrix(int[,] matrix)
+        {
+            Point[] vertexes_coord = new Point[matrix[matrix.GetLength(0) - 2, matrix.GetLength(1) - 2] + 1];
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (matrix[r, c] == -1) continue;
+                    vertexes_coord[matrix[r, c]] = new Point(r - 1, c - 1);
+                }
+            }
+            return vertexes_coord;
+        }
+        public static int[][] GenerateWithHole(int num_of_row, int num_of_col, Point[] start_rect_point,
+                                                                               Point[] end_rect_point,
+                                                                               out Point[] vertexes_coord)
+        {
+            int[,] matrix_graph = GetExpandedMatrix(num_of_row, num_of_col);
+            FillExpandedMatrixBorder(matrix_graph);
+            AddRectangleHoleMatrix(matrix_graph, start_rect_point, end_rect_point);
+            NumberExpandedMatrixWithHole(matrix_graph);
+            vertexes_coord = GetVertexCoordFromMatrix(matrix_graph);
+            return GetGraphFromMatrix(matrix_graph);
         }
     }
 }
---
+++ TestsForAlgorithm/GeneratorTest/GridGraphGenratorTests.cs
@@ -0,0 +1,206 @@
+﻿using ExampleGenerator;
+using GraphRepresentation;
+using System.Drawing;
+
+namespace TestsForAlgorithm.GeneratorTest
+{
+    [TestClass]
+    public class GridGraphGenratorTests
+    {
+        private static bool JaggedArrayAssert (int[][] array, int[][] expected_array)
+        {
+            //Да, мне пришлось писать этот код, так как MSTest не умеет
+            //проверять зубачатые массивы.
+            bool flag_assert = true;
+            if (expected_array.Length != array.Length) flag_assert = false;
+            else
+            {
+                for (int i = 0; i < array.Length && flag_assert; i++)
+                {
+                    if (expected_array[i].Length != array[i].Length)
+                    {
+                        flag_assert = false;
+                        break;
+                    }
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        if (expected_array[i][j] != array[i][j])
+                        {
+                            flag_assert = false;
+                        }
+                    }
+                }
+            }
+            return flag_assert;
+        }
+        [TestMethod]
+        public void GetExpandedMatrix_CreateMatrixSize5to7_MatrixSize7To9()
+        {
+            int num_of_row = 5;
+            int num_of_col = 7;
+            int[,] matrix = GridGraphGenerator.GetExpandedMatrix(num_of_row, num_of_col);
+
+            int expected_num_of_row = 7;
+            int expected_num_of_col = 9;
+
+            Assert.IsTrue(matrix.GetLength(0) == expected_num_of_row && matrix.GetLength(1) == expected_num_of_col);
+        }
+
+        [TestMethod]
+        public void GetExpandedMatrix_CreateMatrixSize2to3_MatrixSize4To5FillZero()
+        {
+            int num_of_row = 2;
+            int num_of_col = 3;
+            int[,] matrix = GridGraphGenerator.GetExpandedMatrix(num_of_row, num_of_col);
+
+            int[,] expected_matrix = {{0, 0, 0, 0, 0},
+                                      {0, 0, 0, 0, 0},
+                                      {0, 0, 0, 0, 0},
+                                      {0, 0, 0, 0, 0}};
+
+            CollectionAssert.AreEqual(matrix, expected_matrix);
+        }
+
+        [TestMethod]
+        public void FillExpandedMatrixBorder_AddMinus1BorderToMatrix_MatrixWithMinus1Border()
+        {
+            int num_of_row = 2;
+            int num_of_col = 3;
+            int[,] matrix = GridGraphGenerator.GetExpandedMatrix(num_of_row, num_of_col);
+            GridGraphGenerator.FillExpandedMatrixBorder(matrix);
+
+            int[,] expected_matrix = {{-1, -1, -1, -1, -1},
+                                      {-1,  0,  0,  0, -1},
+                                      {-1,  0,  0,  0, -1},
+                                      {-1, -1, -1, -1, -1}};
+
+            CollectionAssert.AreEqual(matrix, expected_matrix);
+        }
+
+        [TestMethod]
+        public void AddRectangleHoleMatrix_AddCenterHole_MatrixWithOneCenterHoleMinus1()
+        {
+            int num_of_row = 2;
+            int num_of_col = 3;
+            int[,] matrix = GridGraphGenerator.GetExpandedMatrix(num_of_row, num_of_col);
+            GridGraphGenerator.AddRectangleHoleMatrix(matrix, [new Point(1, 1)], [new Point(2, 3)]);
+
+            int[,] expected_matrix = {{0,  0,  0,  0,  0},
+                                      {0, -1, -1, -1,  0},
+                                      {0, -1, -1, -1,  0},
+                                      {0,  0,  0,  0,  0}};
+
+            CollectionAssert.AreEqual(matrix, expected_matrix);
+        }
+
+        [TestMethod]
+        public void AddRectangleHoleMatrix_AddTwoHole_MatrixWithTwoHoleMinus1()
+        {
+            int num_of_row = 4;
+            int num_of_col = 5;
+            int[,] matrix = GridGraphGenerator.GetExpandedMatrix(num_of_row, num_of_col);
+            GridGraphGenerator.AddRectangleHoleMatrix(matrix, [new Point(1, 1), new Point(4, 4)],
+                                                              [new Point(2, 2), new Point(5, 6)]);
+
+            int[,] expected_matrix = {{0,  0,  0,  0,  0,  0,  0},
+                                      {0, -1, -1,  0,  0,  0,  0},
+                                      {0, -1, -1,  0,  0,  0,  0},
+                                      {0,  0,  0,  0,  0,  0,  0},
+                                      {0,  0,  0,  0, -1, -1, -1},
+                                      {0,  0,  0,  0, -1, -1, -1}};
+
+            CollectionAssert.AreEqual(matrix, expected_matrix);
+        }
+
+        [TestMethod]
+        public void NumberExpandedMatrixWithHole_NumberWithTwoHole_MatrixWithNumner()
+        {
+            int num_of_row = 6;
+            int num_of_col = 7;
+            int[,] matrix = GridGraphGenerator.GetExpandedMatrix(num_of_row, num_of_col);
+            GridGraphGenerator.FillExpandedMatrixBorder(matrix);
+            GridGraphGenerator.AddRectangleHoleMatrix(matrix, [new Point(2, 2), new Point(5, 5)],
+                                                              [new Point(3, 3), new Point(6, 7)]);
+            GridGraphGenerator.NumberExpandedMatrixWithHole(matrix);
+
+            int[,] expected_matrix = {{-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                                      {-1,  0,  1,  2,  3,  4,  5,  6, -1},
+                                      {-1,  7, -1, -1,  8,  9, 10, 11, -1},
+                                      {-1, 12, -1, -1, 13, 14, 15, 16, -1},
+                                      {-1, 17, 18, 19, 20, 21, 22, 23, -1},
+                                      {-1, 24, 25, 26, 27, -1, -1, -1, -1},
+                                      {-1, 28, 29, 30, 31, -1, -1, -1, -1},
+                                      {-1, -1, -1, -1, -1, -1, -1, -1, -1}};
+
+            CollectionAssert.AreEqual(matrix, expected_matrix);
+        }
+
+        [TestMethod]
+        public void GetGraphFromMatrix_GenerateGrphWith8Vertexes_GrphWith8Vertexes()
+        {
+            int num_of_row = 3;
+            int num_of_col = 3;
+            int[,] matrix = GridGraphGenerator.GetExpandedMatrix(num_of_row, num_of_col);
+            GridGraphGenerator.FillExpandedMatrixBorder(matrix);
+            GridGraphGenerator.AddRectangleHoleMatrix(matrix, [new Point(2, 2)],
+                                                              [new Point(2, 2)]);
+            GridGraphGenerator.NumberExpandedMatrixWithHole(matrix);
+            int[][] graph = GridGraphGenerator.GetGraphFromMatrix(matrix);
+
+            int[][] expected_graph = {[1,3],
+                                      [0,2],
+                                      [1,4],
+                                      [0,5],
+                                      [2,7],
+                                      [3,6],
+                                      [5,7],
+                                      [4,6]};
+
+            Assert.IsTrue(JaggedArrayAssert(graph, expected_graph));
+        }
+
+        [TestMethod]
+        public void GenerateWithHole_GenerateGrphWith8Vertexes_GrphWith8Vertexes()
+        {
+            int num_of_row = 3;
+            int num_of_col = 3;
+            Point[] vertexes_coord;
+            int[][] graph = GridGraphGenerator.GenerateWithHole(num_of_row, num_of_col, [new Point(2, 2)],
+                                                                                        [new Point(2, 2)],
+                                                                                        out vertexes_coord);
+
+            int[][] expected_graph = {[1,3],
+                                      [0,2],
+                                      [1,4],
+                                      [0,5],
+                                      [2,7],
+                                      [3,6],
+                                      [5,7],
+                                      [4,6]};
+
+            Assert.IsTrue(JaggedArrayAssert(graph, expected_graph));
+        }
+
+        [TestMethod]
+        public void GenerateWithHole_GenerateGrphWith8Vertexes_CoordForVertexes()
+        {
+            int num_of_row = 3;
+            int num_of_col = 3;
+            Point[] vertexes_coord;
+            int[][] graph = GridGraphGenerator.GenerateWithHole(num_of_row, num_of_col, [new Point(2, 2)],
+                                                                                        [new Point(2, 2)],
+                                                                                        out vertexes_coord);
+
+            Point[] expected_coord = {new(0,0),
+                                      new(0,1),
+                                      new(0,2),
+                                      new(1,0),
+                                      new(1,2),
+                                      new(2,0),
+                                      new(2,1),
+                                      new(2,2)};
+
+            CollectionAssert.AreEqual(vertexes_coord, expected_coord);
+        }
+    }
+}
