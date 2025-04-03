2beac9679e1faa794216766cd7e1de4f8d199c1e
Dekim-49 <74243140+Dekim-49@users.noreply.github.com>
2025-04-03 21:01:45 +0300

    Update Drawer.cs
    1. открыт класс
    2. добавлено масштабирование координат
    3. отрисовывает вершины слева направо сверху вниз.

--- GraphDrawer/Drawer.cs
+++ GraphDrawer/Drawer.cs
@@ -10,114 +10,132 @@ using GraphOptimisation;
 
 namespace GraphDrawer
 {
-    class Drawer : IDrawGraph
+    public class Drawer : IDrawGraph
+{
+    /// <summary>
+    /// Рисует граф
+    /// </summary>
+    /// <param name="graph">Граф в виде двумерного массива</param>
+    /// <param name="coordVertex">Массив координат вершин</param>
+    /// <param name="partition">Разделение</param>
+    /// <param name="filename">полный путь куда будет сохранён файл</param>
+    public void Draw(int[][] graph, Point[] coordVertex, int[] partition, string path)
     {
-        /// <summary>
-        /// Рисует граф
-        /// </summary>
-        /// <param name="graph">Граф в виде двумерного массива</param>
-        /// <param name="coordVertex">Массив координат вершин</param>
-        /// <param name="partition">Разделение</param>
-        /// <param name="filename">Имя файла в которое будет сохранено изображение БЕЗ указания формата</param>
-        public void Draw(int[][] graph, Point[] coordVertex, int[] partition, string filename)
+        foreach (var i in coordVertex)
         {
-            int rad = FindOptimumRadius(coordVertex);
-            int[] size = FrameSize(coordVertex);
-            Bitmap bitmap = new Bitmap(size[0] + rad*2, size[1] + rad*2);
-            Graphics graphics = Graphics.FromImage(bitmap);
-
-            graphics.Clear(Color.White);
-
-            // Рисуются рёбра
-            // Разрез - красный цвет
-            for (int i = 0; i < graph.Length; i++)
+            if (i.X < 100 || i.Y < 100)
             {
-                for (int j = 0; j < graph[i].Length; j++)
+                Scaling(coordVertex);
+                continue;
+            }
+        }
+        int rad = FindOptimumRadius(coordVertex);
+        int[] size = FrameSize(coordVertex);
+        Bitmap bitmap = new Bitmap(size[0] + rad*2, size[1] + rad*2);
+        Graphics graphics = Graphics.FromImage(bitmap);
+
+        graphics.Clear(Color.White);
+
+        // Рисуются рёбра
+        // Разрез - красный цвет
+        for (int i = 0; i < graph.Length; i++)
+        {
+            for (int j = 0; j < graph[i].Length; j++)
+            {
+                if (partition[i] == partition[graph[i][j]])
                 {
-                    if (partition[i] == partition[graph[i][j]])
-                    {
-                        graphics.DrawLine(new Pen(Color.Black, 6), coordVertex[i], coordVertex[graph[i][j]]);
-                    }
-                    else
-                    {
-                        graphics.DrawLine(new Pen(Color.Red, 6), coordVertex[i], coordVertex[graph[i][j]]);
-                    }
+                    graphics.DrawLine(new Pen(Color.Black, rad/5), coordVertex[i].Y, coordVertex[i].X, coordVertex[graph[i][j]].Y, coordVertex[graph[i][j]].X);
+                }
+                else
+                {
+                    graphics.DrawLine(new Pen(Color.Red, rad/5), coordVertex[i].Y, coordVertex[i].X, coordVertex[graph[i][j]].Y, coordVertex[graph[i][j]].X);
                 }
             }
-
-            //Рисуются вершины
-            for (int i = 0; i < graph.Length; i++)
-            {
-                graphics.DrawEllipse(new Pen(Color.Black, 6), coordVertex[i].X - rad, coordVertex[i].Y - rad, rad * 2, rad * 2);
-                if (partition[i] == 0)
-                {
-                    graphics.FillEllipse(Brushes.White, coordVertex[i].X - rad, coordVertex[i].Y - rad, rad * 2, rad * 2);
-                    graphics.DrawString(i.ToString(), new Font("Arial", rad), Brushes.Black, coordVertex[i].X - rad, coordVertex[i].Y - rad);
-                }
-                if (partition[i] == 1)
-                {
-                    graphics.FillEllipse(Brushes.Black, coordVertex[i].X - rad, coordVertex[i].Y - rad, rad * 2, rad * 2);
-                    graphics.DrawString(i.ToString(), new Font("Arial", rad), Brushes.White, coordVertex[i].X - rad, coordVertex[i].Y - rad);
-                }
-            }
-            bitmap.Save(filename + ".png");
         }
 
-        /// <summary>
-        /// Записывает в текстовый файл значения разреза и дисбаланса.
-        /// </summary>
-        /// <param name="graph"></param>
-        /// <param name="partition"></param>
-        /// <param name="filename"></param>
-        public void Statistics(IGraph graph, int[] partition, string filename, bool isStart)
+        //Рисуются вершины
+        for (int i = 0; i < graph.Length; i++)
         {
-            StreamWriter writer;
-            if (isStart)
-                writer = new StreamWriter(filename + ".txt", false);
-            else
-                writer = new StreamWriter(filename + ".txt", true);
-            int criterion = 0;
-            float disbalans = 0;
-            FiducciaMattheysesMethod f = new FiducciaMattheysesMethod();
-            f.Statistics(ref criterion, ref disbalans, graph, partition);
-            writer.WriteLine(DateTime.Now);
-            writer.WriteLine("Разрез: " +  criterion);
-            writer.WriteLine("Дисбаланс: " + disbalans);
-            writer.WriteLine();
-            writer.Close();
-        }
-
-        /// <summary>
-        /// Высчитывает оптимальный размер изображения
-        /// </summary>
-        /// <param name="coordVertex">Массив координат вершин</param>
-        /// <returns>Массив размеров по ширине и высоте соответственно</returns>
-        private int[] FrameSize(Point[] coordVertex)
-        {
-            int[] size = new int[2] { 0, 0 };
-            foreach (var p in coordVertex)
+            graphics.DrawEllipse(new Pen(Color.Black, rad/5), coordVertex[i].Y - rad, coordVertex[i].X - rad, rad * 2, rad * 2);
+            if (partition[i] == 0)
             {
-                if (p.X > size[0]) size[0] = p.X;
-                if (p.Y > size[1]) size[1] = p.Y;
+                graphics.FillEllipse(Brushes.White, coordVertex[i].Y - rad, coordVertex[i].X - rad, rad * 2, rad * 2);
+                graphics.DrawString(i.ToString(), new Font("Arial", rad), Brushes.Black, coordVertex[i].Y - rad, coordVertex[i].X - rad);
             }
-            return size;
-        }
-        /// <summary>
-        /// Высчитывает оптимальный радиус рисуемой вершины
-        /// </summary>
-        /// <param name="coordVertex">Массив координат вершин</param>
-        /// <returns>Оптимальный радиус вершины</returns>
-        private int FindOptimumRadius(Point[] coordVertex)
-        {
-            int[] min = new int[2] { coordVertex[0].X, coordVertex[0].Y };
-            foreach (var p in coordVertex)
+            if (partition[i] == 1)
             {
-                if (p.X < min[0]) min[0] = p.X;
-                if (p.Y < min[1]) min[1] = p.Y;
+                graphics.FillEllipse(Brushes.Black, coordVertex[i].Y - rad, coordVertex[i].X - rad, rad * 2, rad * 2);
+                graphics.DrawString(i.ToString(), new Font("Arial", rad), Brushes.White, coordVertex[i].Y - rad, coordVertex[i].X - rad);
             }
-            return min[0] < min[1] ? min[0]/4 : min[1]/4;
         }
-
-        
+        bitmap.Save(path);
     }
+
+    /// <summary>
+    /// Записывает в текстовый файл значения разреза и дисбаланса.
+    /// </summary>
+    /// <param name="graph"></param>
+    /// <param name="partition"></param>
+    /// <param name="filename"></param>
+    public void Statistics(IGraph graph, int[] partition, string filename, bool isStart)
+    {
+        StreamWriter writer;
+        if (isStart)
+            writer = new StreamWriter(filename + ".txt", false);
+        else
+            writer = new StreamWriter(filename + ".txt", true);
+        int criterion = 0;
+        float disbalans = 0;
+        FiducciaMattheysesMethod f = new FiducciaMattheysesMethod();
+        f.Statistics(ref criterion, ref disbalans, graph, partition);
+        writer.WriteLine(DateTime.Now);
+        writer.WriteLine("Разрез: " +  criterion);
+        writer.WriteLine("Дисбаланс: " + disbalans);
+        writer.WriteLine();
+        writer.Close();
+    }
+
+    /// <summary>
+    /// Высчитывает оптимальный размер изображения
+    /// </summary>
+    /// <param name="coordVertex">Массив координат вершин</param>
+    /// <returns>Массив размеров по ширине и высоте соответственно</returns>
+    private int[] FrameSize(Point[] coordVertex)
+    {
+        int[] size = new int[2] { 0, 0 };
+        foreach (var p in coordVertex)
+        {
+            if (p.X > size[0]) size[0] = p.X;
+            if (p.Y > size[1]) size[1] = p.Y;
+        }
+        return size;
+    }
+    /// <summary>
+    /// Высчитывает оптимальный радиус рисуемой вершины
+    /// </summary>
+    /// <param name="coordVertex">Массив координат вершин</param>
+    /// <returns>Оптимальный радиус вершины</returns>
+    private int FindOptimumRadius(Point[] coordVertex)
+    {
+        int[] min = [coordVertex[0].X, coordVertex[0].Y];
+        foreach (var p in coordVertex)
+        {
+            if (p.X < min[0]) min[0] = p.X;
+            if (p.Y < min[1]) min[1] = p.Y;
+        }
+        return min[0] < min[1] ? min[0]/4 : min[1]/4;
+    }
+    /// <summary>
+    /// Подгоняет размер под пиксели для отрисовки
+    /// </summary>
+    /// <param name="old">Массив координат</param>
+    private void Scaling(Point[] old)
+    {
+        for (int i = 0; i < old.Length; i++)
+        {
+            old[i].X = (old[i].X + 1) * 100;
+            old[i].Y = (old[i].Y + 1) * 100;
+        }
+    }
+}
 }
