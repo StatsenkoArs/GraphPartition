52096d1b37529aef572f4613a1bdc802ca577952
Фёдор Трошанин <trosio2003@gmai.com>
2025-06-06 16:41:36 +0300

    Добавлены классы для создания порядка обхода при паросочетании

---
+++ GraphReduction/GraphTraversal.cs
@@ -0,0 +1,39 @@
+﻿using GraphRepresentation;
+
+namespace GraphReduction
+{
+    public interface IGraphTraversal
+    {
+        int[] GetGraphTraversal(IGraph graph);
+    }
+
+    public class RandGraphTraversal : IGraphTraversal
+    {
+        public int[] GetGraphTraversal(IGraph graph)
+        {
+            int[] rand_vert_traversal = new int[graph.CountVertecies];
+            for (int i = 0; i < graph.CountVertecies; i++)
+            {
+                rand_vert_traversal[i] = i;
+            }
+            Random.Shared.Shuffle(rand_vert_traversal);
+            return rand_vert_traversal;
+        }
+    }
+
+    public class VertexWeightTraversal : IGraphTraversal
+    {
+        public int[] GetGraphTraversal(IGraph graph)
+        {
+            int[] vert_traversal = new int[graph.CountVertecies];
+            int[] vert_weight = new int[graph.CountVertecies];
+            for (int i = 0; i < graph.CountVertecies; i++)
+            {
+                vert_traversal[i] = i;
+                vert_weight[i] = graph.GetVertexWeight(i);
+            }
+            Array.Sort(vert_weight, vert_traversal);
+            return vert_traversal;
+        }
+    }
+}
