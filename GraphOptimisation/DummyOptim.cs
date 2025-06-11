41cf5de985c6d84df5e6f966de5783517aeef969
Фёдор Трошанин <trosio2003@gmai.com>
2025-06-11 11:55:58 +0300

    Добавлена заглушка для визуализатора

---
+++ GraphOptimisation/DummyOptim.cs
@@ -0,0 +1,17 @@
+﻿using GraphRepresentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphOptimisation
+{
+    public class DummyOptim : IPartitionOptimisation
+    {
+        public int[] OptimisePartition(IGraph graph, int[] partition, int numberOfBlockedIterations, int limitDisbalance)
+        {
+            return partition;
+        }
+    }
+}
